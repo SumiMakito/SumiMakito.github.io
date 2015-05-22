b620a63ad8106d2ef565f784c4e62e9163055057
sumimakito <sumimakito@hotmail.com>
2015-05-22 20:36:42 +0800

    Big than Gib.

---
+++ js/mkto.js
@@ -0,0 +1,7 @@
+function optimize() {
+	var margin = ($(window).height() - $('#main').height() - 15 - 8 - $('#footer').height()) / 2;
+	$('#main').css('margin-top', margin).css('margin-bottom', margin);
+	var marginxs = ($(window).height() - $('#main-xs').height() - 15 - 8 - $('#footer').height()) / 2;
+	$('#main-xs').css('margin-top', marginxs).css('margin-bottom', marginxs);
+	$('div').css('opacity', 1);
+};
