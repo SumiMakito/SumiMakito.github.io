c65b5ee0595d65ff9108d7ce1e02a0669605a373
sumimakito <sumimakito@hotmail.com>
2015-05-23 17:07:40 +0800

    CSS and JS updated.

--- js/mkto.js
+++ js/mkto.js
@@ -3,5 +3,7 @@ function optimize() {
 	$('#main').css('margin-top', margin).css('margin-bottom', margin);
 	var marginxs = ($(window).height() - $('#main-xs').height() - 15 - 8 - $('#footer').height()) / 2;
 	$('#main-xs').css('margin-top', marginxs).css('margin-bottom', marginxs);
-	$('div').css('opacity', 1);
+	$('#preload').hide(function(){
+		$('div').css('opacity', 1);
+	});
 };
