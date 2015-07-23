6ba97acd0b2f3d7b2949ffd48a7c23eed5936c19
SumiMakito <sumimakito@hotmail.com>
2015-07-23 12:15:23 +0800

    Static generator

--- build.sh
+++ build.sh
@@ -1,12 +1,14 @@
 wget http://static.keep.moe/Crystal.py
 python Crystal.py generate https://github.com/SumiMakito/SumiMakito.github.io.git ./
-find
-cp -rf /tmp/crystal_tmp/dist/* ./
+#find
+#cp -rf /tmp/crystal_tmp/dist/* ./
+cd /tmp/crystal_tmp/dist
+rm -rf ./.git
 git init
 git config --global push.default matching
 git config --global user.email "sumimaito@hotmail.com"
 git config --global user.name "SumiMakito"
 git add --all .
 git commit -m "Crystal Auto Builder"
-git push --force https://${GitHubKEY}@github.com/SumiMakito/SumiMakito.github.io.git raw:master
+git push --force https://${GitHubKEY}@github.com/SumiMakito/SumiMakito.github.io.git master
 
