a9bd5051861cf058111127faa0f7ce3678730650
SumiMakito <sumimakito@hotmail.com>
2015-07-23 12:04:48 +0800

    Static generator

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 wget http://static.keep.moe/Crystal.py
 python Crystal.py generate https://github.com/SumiMakito/SumiMakito.github.io.git ./
 cp -rf /tmp/crystal_tmp/dist/* ./
+git config --global push.default matching
 git config --global user.email "sumimaito@hotmail.com"
 git config --global user.name "SumiMakito"
 git add --all .
