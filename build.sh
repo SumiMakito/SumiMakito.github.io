fa7d80c1f606922a842524f38446c4b5f2c0c543
SumiMakito <sumimakito@hotmail.com>
2015-11-06 14:10:33 +0800

    Fixed some problems

--- build.sh
+++ build.sh
@@ -1,10 +1,9 @@
 hexo generate
 cd ./public
-cp ../../.blocked_travis.yml ./.travis.yml
 git init
 git config --global push.default matching
 git config --global user.email "sumimakito@hotmail.com"
 git config --global user.name "SumiMakito"
 git add --all .
 git commit -m "Travis CI Auto Builder"
-git push --quiet --force https://${GitHubKEY}@github.com/SumiMakito/SumiMakito.github.io.git hexo
+git push --quiet --force https://${GitHubKEY}@github.com/SumiMakito/SumiMakito.github.io.git master
