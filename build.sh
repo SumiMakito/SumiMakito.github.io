1080264363e8650411fcbbd2d66ec82eeec3a5e2
SumiMakito <sumimakito@hotmail.com>
2015-11-06 13:34:37 +0800

    Applied CI

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 hexo generate
-#git init
-#git config --global push.default matching
-#git config --global user.email "sumimakito@hotmail.com"
-#git config --global user.name "SumiMakito"
-#git add --all .
-#git commit -m "Travis CI Auto Builder"
-#git push --quiet --force https://${GitHubKEY}@github.com/SumiMakito/SumiMakito.github.io.git master
-find ./
+cd ./public
+git init
+git config --global push.default matching
+git config --global user.email "sumimakito@hotmail.com"
+git config --global user.name "SumiMakito"
+git add --all .
+git commit -m "Travis CI Auto Builder"
+git push --quiet --force https://${GitHubKEY}@github.com/SumiMakito/SumiMakito.github.io.git master
