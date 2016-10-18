a3d81ec4c1517e23566ae285b460c81096e21e3a
Makito <sumimakito@hotmail.com>
2016-10-18 17:31:43 +0800

    Redirect to Keep.moe

--- v1.v2.v3/v3/js/mkto.js
+++
@@ -1,8 +0,0 @@
-function optimize() {
-	var margin = ($(window).height() - $('#main').height() - 15 - 8 - $('#footer').height()) / 2;
-	$('#main').css('margin-top', margin).css('margin-bottom', margin);
-	var marginxs = ($(window).height() - $('#main-xs').height() - 15 - 8 - $('#footer').height()) / 2;
-	$('#main-xs').css('margin-top', marginxs).css('margin-bottom', marginxs);
-	$('#preload').hide();
-	$('div').css('opacity', 1);
-};
